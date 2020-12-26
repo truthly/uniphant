2c2a7bf8e65bdcb824e0d49984b63a5e21c8b467
Joel Jakobsson <joel@compiler.org>
2020-12-26 16:50:04 +0100

    Avoid need to cd .. by using subshell (cd ....)

--- install.sh
+++ install.sh
@@ -8,35 +8,20 @@ sudo service postgresql start
 sudo -u postgres createuser -s "$USER"
 createdb uniphant
 git clone https://github.com/truthly/pg-cbor.git
-cd pg-cbor || exit
-make
-sudo make install
-make installcheck
-cd ..
+(cd pg-cbor && make && sudo make install && make installcheck)
 git clone https://github.com/ameensol/pg-ecdsa.git
-cd pg-ecdsa || exit
-PG_CFLAGS=-Wno-vla make
-sudo make install
-make installcheck
-cd ..
+(cd pg-ecdsa && PG_CFLAGS=-Wno-vla make && sudo make install && make installcheck)
 git clone https://github.com/truthly/pg-webauthn.git
-cd pg-webauthn || exit
-make
-sudo make install
-make installcheck
-cd ..
+(cd pg-webauthn && make && sudo make install && make installcheck)
 wget --quiet https://github.com/PostgREST/postgrest/releases/download/v7.0.1/postgrest-v7.0.1-linux-x64-static.tar.xz
 tar xvf postgrest-v7.0.1-linux-x64-static.tar.xz
 sudo cp postgrest /bin/postgrest
-git clone https://github.com/truthly/uniphant.git
-cd uniphant || exit
-make
-sudo make install
 createuser api -L -s
 createuser web_anon -L
 createuser postgrest -I -l
+git clone https://github.com/truthly/uniphant.git
+(cd uniphant && make && sudo make install && make installcheck)
 psql -c "CREATE EXTENSION uniphant CASCADE" uniphant
-make installcheck
 sudo mkdir -p /etc/postgrest
 sudo cp postgrest.conf /etc/postgrest/config
 sudo cp postgrest.service /etc/systemd/system/postgrest.service
