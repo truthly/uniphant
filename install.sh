c52c8484edefba1b519a9e42adb129140f6a65d9
Joel Jakobsson <joel@compiler.org>
2020-12-26 14:41:01 +0100

    Use 'cd ... || exit' in case cd fails
    Also double quote $USER to prevent globbing and word splitting

--- install.sh
+++ install.sh
@@ -4,22 +4,22 @@ sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get -y install postgresql postgresql-server-dev-13 build-essential nginx-light
-sudo -u postgres createuser -s $USER
+sudo -u postgres createuser -s "$USER"
 createdb uniphant
 git clone https://github.com/truthly/pg-cbor.git
-cd pg-cbor
+cd pg-cbor || exit
 make
 sudo make install
 make installcheck
 cd ..
 git clone https://github.com/ameensol/pg-ecdsa.git
-cd pg-ecdsa
+cd pg-ecdsa || exit
 PG_CFLAGS=-Wno-vla make
 sudo make install
 make installcheck
 cd ..
 git clone https://github.com/truthly/pg-webauthn.git
-cd pg-webauthn
+cd pg-webauthn || exit
 make
 sudo make install
 make installcheck
@@ -28,7 +28,7 @@ wget --quiet https://github.com/PostgREST/postgrest/releases/download/v7.0.1/pos
 tar xvf postgrest-v7.0.1-linux-x64-static.tar.xz
 sudo cp postgrest /bin/postgrest
 git clone https://github.com/truthly/uniphant.git
-cd uniphant
+cd uniphant || exit
 make
 sudo make install
 createuser api -L -s
@@ -45,5 +45,5 @@ sudo systemctl start postgrest
 sudo rm /etc/nginx/sites-enabled/default
 sudo cp nginx.conf /etc/nginx/sites-available/uniphant
 sudo ln -s /etc/nginx/sites-available/uniphant /etc/nginx/sites-enabled/uniphant
-sudo ln -s $HOME/uniphant/demo /var/www/html/uniphant
+sudo ln -s "$HOME/uniphant/demo" /var/www/html/uniphant
 sudo systemctl restart nginx
