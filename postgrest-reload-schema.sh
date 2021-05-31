5c45360d9081d0496d183b75309803a31f26100b
Joel Jakobsson <joel@compiler.org>
2021-05-31 04:30:30 +0200

    Don't hard-code "uniphant" as database might be named something else

--- postgrest-reload-schema.sh
+++ postgrest-reload-schema.sh
@@ -2,5 +2,5 @@
 killall -SIGUSR1 postgrest
 sleep 1
 JSON=$(curl -s -H 'Content-Type: application/json' 'http://localhost:3000/')
-echo "SELECT set_openapi_swagger(:'json')" | psql -X -v json="$JSON" uniphant
-psql -X -c "SELECT auto_add_new_resources()" uniphant
+echo "SELECT set_openapi_swagger(:'json')" | psql -X -v json="$JSON"
+psql -X -c "SELECT auto_add_new_resources()"
