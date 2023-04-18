7cbd4bb528f83de98db2c219cf87feec517d38b6
Joel Jakobsson <joel@compiler.org>
2023-04-18 16:49:53 +0200

    Remove files put in the wrong place by mistake

--- py/example_workers/workers/worker_manager/list.py
+++
@@ -1,9 +0,0 @@
-    # Handle `list` command.
-    #
-    # The list command isn't worker_id specific and is therefore
-    # handled before worker_id is set.
-    if command == 'list':
-        worker_ids = ensure_worker_exists_and_get_ids(config, connection)
-        for worker_id in worker_ids:
-            print(worker_id + " " + worker_type)
-        sys.exit(0)
