fb5f575d39e3792c1242012de3b2f8377bc269e4
Joel Jakobsson <joel@compiler.org>
2023-04-24 09:54:11 +0200

    Create uniphant database and user in the example_workers database.sql script.

--- py/example_workers/database.sql
+++ py/example_workers/database.sql
@@ -1,3 +1,7 @@
+CREATE USER uniphant WITH SUPERUSER;
+CREATE DATABASE uniphant WITH OWNER = uniphant;
+\c uniphant
+CREATE EXTENSION IF NOT EXISTS uniphant CASCADE;
 BEGIN;
 \ir database/public.sql
 \ir database/opentdb.sql
