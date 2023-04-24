1226a06c43c00c4c220a95661ea77795c658cf8c
Joel Jakobsson <joel@compiler.org>
2023-04-24 10:27:03 +0200

    Use named parameters for DB queries.

--- py/uniphant/database.py
+++ py/uniphant/database.py
@@ -22,42 +22,42 @@ def connect(dbname: Optional[str] = None,
 def register_host(connection: Connection, host_id: UUID, host_name: str) -> None:
     connection.cursor().execute("""
         SELECT register_host(
-            host_id := %s,
-            host_name := %s
+            host_id := %(host_id)s,
+            host_name := %(host_name)s
         )
-    """, (str(host_id), host_name))
+    """, {"host_id": str(host_id), "host_name": host_name})
 
 def register_process(connection: Connection, process_id: UUID, worker_id: UUID, pid: int) -> None:
     connection.cursor().execute("""
         SELECT register_process(
-            process_id := %s,
-            worker_id := %s,
-            pid := %s
+            process_id := %(process_id)s,
+            worker_id := %(worker_id)s,
+            pid := %(pid)s
         )
-    """, (str(process_id),str(worker_id),pid))
+    """, {"process_id": str(process_id), "worker_id": str(worker_id), "pid": pid})
 
 def keepalive(connection: Connection, process_id: UUID) -> bool:
     cursor = connection.cursor()
     cursor.execute("""
-        SELECT keepalive(process_id := %s)
-    """, (str(process_id),))
-    should_run: bool = cursor.fetchone()[0]
+        SELECT keepalive(process_id := %(process_id)s)
+    """, {"process_id": str(process_id)})
+    should_run = cursor.fetchone()[0]
     return should_run
 
 def delete_process(connection: Connection, process_id: UUID) -> None:
     connection.cursor().execute("""
-        SELECT delete_process(process_id := %s)
-    """, (str(process_id),))
+        SELECT delete_process(process_id := %(process_id)s)
+    """, {"process_id": str(process_id)})
 
 def get_worker_process_id(connection: Connection, worker_id: UUID) -> UUID:
     cursor = connection.cursor()
     cursor.execute("""
-        SELECT get_worker_process_id(worker_id := %s)
-    """, (str(worker_id),))
-    process_id: UUID = cursor.fetchone()[0]
+        SELECT get_worker_process_id(worker_id := %(worker_id)s)
+    """, {"worker_id": str(worker_id)})
+    process_id = cursor.fetchone()[0]
     return process_id
 
 def request_process_termination(connection: Connection, process_id: UUID) -> None:
     connection.cursor().execute("""
-        SELECT request_process_termination(process_id := %s)
-    """, (str(process_id),))
+        SELECT request_process_termination(process_id := %(process_id)s)
+    """, {"process_id": str(process_id)})
